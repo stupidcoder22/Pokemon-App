b665e615fbc7ce9a37d048aea3763c9bf54c65ef
prateek <stupidcoder22@gmail.com>
2022-05-23 21:52:54 +0530

    done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import logo from "./img.png";
 
-const Card = () => {
+const Card = ({ pokemon, loading, infoPoke }) => {
+  console.log(pokemon);
   return (
-    <div className="card">
-      <h2>1</h2>
-      <img src="./img.png" alt="" />
-      <h2>Charmender</h2>
-    </div>
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        pokemon.map((item, index) => {
+          return (
+            <div className="card" key={item.id} onClick={() => infoPoke(item)}>
+              <h2>{item.id}</h2>
+              <img src={item.sprites.front_default} alt="" />
+              <h2>{item.name}</h2>
+            </div>
+          );
+        })
+      )}
+    </>
   );
 };
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,73 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
 const Main = () => {
+  const [pokeData, setpokeData] = useState([]);
+  const [loading, setloading] = useState();
+  const [url, seturl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextUrl, setnextUrl] = useState();
+  const [prevUrl, setprevUrl] = useState();
+  const [pokeDex, setpokeDex] = useState();
+
+  const pokefun = async () => {
+    setloading(true);
+    const res = await axios.get(url);
+    setnextUrl(res.data.next);
+    setprevUrl(res.data.previous);
+    getPokemon(res.data.results);
+    setloading(false);
+  };
+
+  const getPokemon = async (res) => {
+    res.map(async (item) => {
+      const res = await axios.get(item.url);
+      setpokeData((state) => {
+        state = [...state, res.data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+      });
+    });
+  };
+
+  useEffect(() => {
+    pokefun();
+  }, [url]);
+
   return (
     <div className="container">
       <div className="left-content">
-        <Card />
+        <Card
+          pokemon={pokeData}
+          loading={loading}
+          infoPoke={(poke) => setpokeDex(poke)}
+        />
+        <div className="btn-group">
+          {prevUrl && (
+            <button
+              onClick={() => {
+                setpokeData([]);
+                seturl(prevUrl);
+              }}
+            >
+              previous
+            </button>
+          )}
+          {nextUrl && (
+            <button
+              onClick={() => {
+                setpokeData([]);
+                seturl(nextUrl);
+              }}
+            >
+              next
+            </button>
+          )}
+        </div>
       </div>
-      <div className="right-content"></div>
+      <div className="right-content">
+        <Pokeinfo data={pokeDex} />
+      </div>
     </div>
   );
 };
